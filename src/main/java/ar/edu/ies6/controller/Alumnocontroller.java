0332812fdcf05bd48a5fad0316db8639cefb756e
Usuario <Usuario@DESKTOP-6LQ5IE7>
2023-10-25 19:53:35 -0300

    Ramos Vanesa Elizabeth Commit Message cambios y errores

--- src/main/java/ar/edu/ies6/controller/Alumnocontroller.java
+++ src/main/java/ar/edu/ies6/controller/Alumnocontroller.java
@@ -1,7 +1,40 @@
 package ar.edu.ies6.controller;
 
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.ies6.model.*;
+
 @Controller
 public class Alumnocontroller {
+	@GetMapping({"/index","/","home","/alumno"})
+	public ModelAndView cargarAlumno() {
+		Alumno alu= new Alumno ();
+		alu.setfechaNac(LocalDate.of(1988, 5, 14));
+		System.out.println("edad:"*alu.getedad());
+		ModelAndView modelView= new ModelAndView("index");
+		modelView.addObject("Alumno",alu);
+		return modelView;
+	}
+		
+		
+		
+	
+		@PostMapping("/cargarAlumno")
+		public ModelAndView cargarAlumno(@ModelAttribute("alumno")Alumno alumno) {
+			
+			Listado.add(alumno);
+			ModelAndView modelView=new ModelAndView("listadodealumno");
+		
+			modelView.addObject("listado", listado);
+			return modelView;
+		}
+		
+	}
 
 	
 	
--- src/main/java/ar/edu/ies6/model/Alumno.java
+++ src/main/java/ar/edu/ies6/model/Alumno.java
@@ -1,8 +1,78 @@
 package ar.edu.ies6.model;
 
-import java.time.Localdate
+import java.time.LocalDate;
+import java.time.Period;
+
+@component
 public class Alumno {
-	private String name/
-	private String lastname
+	private String name;
+	private String lastName;
+	private String addressStreet;
+	private String addressNumber;
+	private LocalDate fechaNac;
+	private boolean estado;
+	
+public Alumno() {
+	// TODO Auto-generated constructor stub
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getLastName() {
+	return lastName;
+}
+
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+
+public String getAddressStreet() {
+	return addressStreet;
+}
+
+public void setAddressStreet(String addressStreet) {
+	this.addressStreet = addressStreet;
+}
+
+public String getAddressNumber() {
+	return addressNumber;
+}
+
+public void setAddressNumber(String addressNumber) {
+	this.addressNumber = addressNumber;
+}
+
+public LocalDate getFechaNac() {
+	return fechaNac;
+}
+
+public void setFechaNac(LocalDate fechaNac) {
+	this.fechaNac = fechaNac;
+}
+
+public boolean isEstado() {
+	return estado;
+}
+
+public void setEstado(boolean estado) {
+	this.estado = estado;
+}
+
+public Integer getEdad () {
+	//TODO calcularEdad
+		int edad=0;
+	//LocalDate fechaN= fechaNac.toInstant ().atZone (ZoneId.systemDefault()).toLocalDate();
+		LocalDate fechaActual = LocalDate.now();
+	Period periodo= Period.between (fechaNac, fechaActual);
+			edad=periodo.getYears();
+	return edad;
+	}
+	
 
 }
